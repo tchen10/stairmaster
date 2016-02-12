a330b49cff021cd3eb86130722179cdf43393f3e
Tiffany Chen <tchen@thoughtworks.com>
2016-02-11 19:26:15 -0500

    [8] Renamed team tab to settings.

--- app/features/pairs/pairsCtrl.js
+++ app/features/pairs/pairsCtrl.js
@@ -5,7 +5,9 @@ var Firebase = require('firebase');
 angular.module('stairmaster.pairs.pairs-controller', [require('angularfire')])
 
 .controller('PairsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-    var ref = new Firebase('https://stairmaster.firebaseio.com/Pairs');
-    $scope.pairs = $firebaseArray(ref);
+    var pairsRef = new Firebase('https://stairmaster.firebaseio.com/Pairs');
+    $scope.pairs = $firebaseArray(pairsRef);
+    var personsRef = new Firebase('https://stairmaster.firebaseio.com/Persons');
+    $scope.persons = $firebaseArray(personsRef);
 
 }]);
